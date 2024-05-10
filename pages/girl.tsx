2a4db3edf55463f8cce95dc14aec579a00580838
tranlap2412 <87066648+tranlap2412@users.noreply.github.com>
2024-05-10 14:16:13 +0700

    test img

--- pages/girl.tsx
+++ pages/girl.tsx
@@ -117,7 +117,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
                 placeholder="blur"
                 loading="lazy"
                 blurDataURL={blurDataUrl}
-                src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/${public_id}.${format}`}
+                src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${public_id}.${format}`}
                 width={720}
                 height={480}
                 sizes="(max-width: 640px) 100vw,
